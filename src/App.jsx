f5740330aca95fc8bf6e047cfffae602c1b227c7
Antonina Honcharenko <104636066+AntoninaHoncharenko@users.noreply.github.com>
2023-07-06 23:03:51 +0200

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { findUniqueSymbol } from "./utils/findSymbol";
+import css from "../src/App.module.css";
 
 function App() {
   const ref = useRef(null);
@@ -24,21 +25,37 @@ function App() {
   };
 
   return (
-    <>
-      <h1>Find your unique symbol</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="text">Enter a text</label>
-        <textarea id="text" ref={ref}></textarea>
-        <button type="submit">Find</button>
-        <button type="button" onClick={handleReset}>
-          Reset
-        </button>
-      </form>
-      <div>
-        <p>Unique Symbol:</p>
-        {uniqueSymbol && <span>{uniqueSymbol}</span>}
+    <section className={css.section}>
+      <div className={css.container}>
+        <h1 className={css.title}>Find your unique symbol</h1>
+        <form onSubmit={handleSubmit} className={css.form}>
+          <textarea
+            id="text"
+            ref={ref}
+            placeholder="Enter a text"
+            className={css.textarea}
+          ></textarea>
+          <div className={css.btn_wrap}>
+            <button type="submit" className={css.btn}>
+              Find
+            </button>
+            <button type="button" onClick={handleReset} className={css.btn}>
+              Reset
+            </button>
+          </div>
+        </form>
+        <div className={css.text_wrap}>
+          <p className={css.text}>Unique Symbol:</p>
+          <div className={css.symbol_wrap}>
+            {uniqueSymbol ? (
+              <p className={css.symbol}>{uniqueSymbol}</p>
+            ) : (
+              <p className={css.symbol}>?</p>
+            )}
+          </div>
+        </div>
       </div>
-    </>
+    </section>
   );
 }
 
--- src/utils/findSymbol.js
+++ src/utils/findSymbol.js
@@ -1,32 +1,32 @@
 export const findUniqueSymbol = (text) => {
-    let wordArray = text.split(" ");
-    let symbols = [];
-    let uniqueSymbol = null;
+  let wordArray = text.trim().split(" ");
+  let symbols = [];
+  let uniqueSymbol = null;
 
-    for (let word of wordArray) {
-      let charCount = {};
+  for (let word of wordArray) {
+    let charCount = {};
 
-      for (let char of word) {
-        if (!(char in charCount)) {
-          charCount[char] = 0;
-        }
-        charCount[char] += 1;
+    for (let char of word) {
+      if (!(char in charCount)) {
+        charCount[char] = 0;
       }
-
-      for (let char of word) {
-        if (charCount[char] === 1) {
-            symbols.push(char);
-          break;
-        }
-      }
+      charCount[char] += 1;
     }
 
-    for (let symbol of symbols) {
-      if (!symbols.includes(symbol, symbols.indexOf(symbol) + 1)) {
-        uniqueSymbol = symbol;
+    for (let char of word) {
+      if (charCount[char] === 1) {
+          symbols.push(char);
         break;
       }
     }
+  }
 
-    return uniqueSymbol;
-  };
+  for (let symbol of symbols) {
+    if (!symbols.includes(symbol, symbols.indexOf(symbol) + 1)) {
+      uniqueSymbol = symbol;
+      break;
+    }
+  }
+
+  return uniqueSymbol;
+};
